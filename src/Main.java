714c41f6284db3a6d17875354168a7e3c3661476
Дмитрий Долиденок <dolidenok@MacBook-Air-Dmitrij.local>
2020-02-19 22:21:33 +0300

    Program ready

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        long ticketCost = 15000;
+        long miles = ticketCost / 20;
+        System.out.println(miles);
     }
 }
